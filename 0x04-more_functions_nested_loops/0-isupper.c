3771b14c76cc5a3c1303f0e26ee58e0321cf6134
anenae <84938376+anenae@users.noreply.github.com>
2021-06-24 18:22:10 +0200

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,31 +1,16 @@
 #include "holberton.h"
 
-
-
 /**
-
  * _isupper - check uppercase character.
-
  * @c: integer.
-
  * Return: 0 succes.
-
  */
-
 int _isupper(int c)
-  
 {
-  
-  if ((c >= 65) && (c <= 90))
-    
-    {
-      
-      return (1);
-      
-    }
-  
-  else
-    
-    return (0);
-  
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
