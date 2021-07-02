8a9801300c0b69cb6b4aff5d5c05e93d974f4015
anenae <84938376+anenae@users.noreply.github.com>
2021-07-02 09:18:08 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a =-1, i;
-	for (i = 0; dest[i] != '\0';i++)
-	;
-	
-	do {
-		a++;
-		dest[i] = src[a];
-		i++;
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
 	}
-	while(src[a] != '\0');
-	return (dest);
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
