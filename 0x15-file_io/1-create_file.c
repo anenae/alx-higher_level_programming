1401efb41ab1553987d107c02fc0a755213fd6b0
anenae <84938376+anenae@users.noreply.github.com>
2021-08-12 14:08:16 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,36 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  create_file - create a file and write in it
- * @filename:file to be created
- * @text_content: content of the file created
- * Return: 1 or -1;
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the content to be written into the file
+ * Return: 1 on success or -1 on error
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, w;
+	int fd, i, count = 0;
+	ssize_t wr;
 
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	/*add a check; if file exists, truncate it*/
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-	{
 		return (-1);
-	}
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
-		;
-		w = write(fd, text_content, len);
+		close(fd);
+		return (1);
 	}
-	if (w == -1)
-	{
+	for (i = 0; text_content[i] != '\0'; i++)
+		count++;
+
+	wr = write(fd, text_content, count);
+	if (wr == -1)
 		return (-1);
-	}
+
 	close(fd);
+
 	return (1);
 }
